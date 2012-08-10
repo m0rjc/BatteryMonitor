9954ffbaba45185fb6ddb33665449769d4a1e597
Richard Corfield <m0rjc@m0rjc.me.uk>
2012-08-10 17:27:41 +0100

    More work on a main loop
    Developed the main loop, but will consider switching to an interrupt
    driven approach which will let me run the thing in SLEEP most of the
    time. Means it's sampling more frequently. Should mean smaller code and
    interesting options for making it programmable later.

--- BatteryMonitor.asm
+++ BatteryMonitor.asm
@@ -19,17 +19,22 @@ main
 
 ; Not using interrupts
 	CLRF	INTCON
-
-; Set up timer 1 for fairly infrequent polling
 	movlw	b'10001111'
 	movwf	OPTION
+
+; Set up timer 1 for fairly infrequent polling  (1MHz / 8 / 256 / 256 = approx 2Hz)
+	BANKSEL T1CON
+	MOVLW b'00110101'  ; Enabled, Internal Clock, Prescale 8
+	MOVWF T1CON
+	CLRF	TMR1L
+	CLRF	TMR1H
 
 ; Set up GPIO - Analog 0 and 4 outputs
 	movlw	b'00001001'
 	tris	GPIO
 
 ; Set up ADC to read GP0, use top 2 bits and bottom 8 bits.
-
+; Use internal timer. Then I can change this code to spend most of its time in sleep!
 
 ; Populate the tables
 	movlw	18
@@ -49,11 +54,13 @@ populateLoop
 ; Main Loop
 ;--------------------------------------------------------------------------------
 mainLoop
-; Sit polling timer 1. When it overflows trigger an analog reading
 
-
-; Sit polling the ADC. When ready update the LEDs.
-
+; Trigger the ADC and poll for completion. 
+	BANKSEL ADCON0
+	BSF		ADCON0	1		; GO
+adcPoll
+	BTFSC	ADCON0	1		; ¬DONE
+	GOTO adcPoll
 
 ; Load the low bits of the ADC result into tmp
 	BANKSEL ADRESL
@@ -65,7 +72,7 @@ mainLoop
 ; If the result was too low then we set tmp to 0 to trigger a hit on the first test
 	BANKSEL ADRESH
 	movlw	3
-	subwf	ADRESH
+	subwf	ADRESH, W
 	BANKSEL tmp
 	btfss	STATUS, C
 	clrf	tmp
@@ -77,7 +84,7 @@ subtractLoop
 	movf	INDF, W		; Reads the subtract value
 	btfsc	STATUS, Z	; Check for the terminator
 		goto resultFound	; Terminator means use that result
-	subwf	tmp
+	subwf	tmp, F
 	btfss	STATUS, C
 		goto resultFound
 	incf FSR
@@ -89,6 +96,13 @@ resultWasFound
 	movf	INDF, W
 	movwf	GPIO
 	
+; Sit polling timer 1. 
+	BANKSEL PIR1
+	BCF		PIR1
+tmrPoll
+	BTFSS	PIR1	0
+	GOTO tmrPoll
+
 	goto mainLoop
 
 ;--------------------------------------------------------------------------------
@@ -105,10 +119,10 @@ progData	CODE
 ; With a 1/3 divider from the supply voltage we get
 ;
 ;	Threshold	Input to ADC	Top 2 bits	Bottom 8 bits	Subtract
-;	infinite	>4.90										terminator	Overcharge															
-;	14.7		4.90				3			234			20		   	Topping charge AGM											
-;	14.4		4.80				3			214			35		   	Topping charge
-;	13.9		4.60				3			173			81		   	Float (should be 13.8)  													
+;	infinite	>4.93										terminator	Overcharge															
+;	14.8		4.93				3			241			21		   	Topping charge AGM (14.7)										
+;	14.5		4.83				3			220			41		   	Topping charge (14.4)
+;	13.9		4.63				3			179			81		   	Float (should be 13.8)  													
 ;	12.7		4.23				3			98			31		   	100%												
 ;	12.25		4.08				3			67			17			 75%												
 ;	12	    	4.00				3			50			17			 50%												
@@ -140,13 +154,13 @@ romTables	ADDWF PCL
 	RETLW 31
 	RETLW	b'00110100'			; Up to 100%		 		   GREEN
 
-	RETLW 75
+	RETLW 81
 	RETLW	b'00010100'			; Float charge up to to 13.8V  GREEN             BLUE
 
 	RETLW 41
 	RETLW	b'00000000'			; Boost for wet cell to 14.4V        ORANGE      BLUE
 
-	RETLW 20
+	RETLW 21
 	RETLW	b'00000010'			; Boost for AGM cell to 14.7V        ORANGE RED  BLUE
 
 	RETLW 0		; Terminator
